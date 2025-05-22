6e2bf090bfa9235483046a3597f532bdafff1d6c
davidguzman2104 <l21200255@pachuca.tecnm.mx>
2025-05-22 10:53:57 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
             msgText.innerHTML = "";
 
             try {
-                const response = await fetch("http://3.91.53.162/api-gpt-php/endpoints/chat.php", {
+                const response = await fetch("http://44.198.166.166/api-gpt-php/endpoints/chat.php", {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
